cc882f98fdd80f2ace22d2c311e7317914b6acb5
Matt Hill <mattnine@protonmail.com>
2026-04-03 12:37:26 -0600

    trust the start-os proxy

--- startos/fileModels/configuration.yaml.ts
+++ startos/fileModels/configuration.yaml.ts
@@ -1,11 +1,22 @@
 import { FileHelper, z } from '@start9labs/start-sdk'
 import { sdk } from '../sdk'
 
-const shape = z
-  .object({
-    default_config: z.object({}).catch({}),
-  })
-  .strip()
+const trustedProxy = '10.0.3.0/24' as const
+
+const httpDefaults = {
+  use_x_forwarded_for: true as const,
+  trusted_proxies: [trustedProxy],
+}
+
+const shape = z.object({
+  default_config: z.object({}).catch({}),
+  http: z
+    .object({
+      use_x_forwarded_for: z.literal(httpDefaults.use_x_forwarded_for).catch(httpDefaults.use_x_forwarded_for),
+      trusted_proxies: z.array(z.literal(trustedProxy)).catch(httpDefaults.trusted_proxies),
+    })
+    .catch(httpDefaults),
+})
 
 export const configurationYaml = FileHelper.yaml(
   {
